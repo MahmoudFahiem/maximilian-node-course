ff795313afedd983840420a08dc9e0fad94fa673
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-20 22:27:21 +0200

    Node modules

--- server.js
+++ server.js
@@ -1,52 +1,10 @@
 const http = require("http");
-const fs = require("fs");
 
-/**
- *  Event Loop
- *  - A loop that is run or start by nodejs that keeps it process running
- *  - And all callbacks by a certain order
- */
+const routes = require("./routes");
 
 const server = http.createServer((req, res) => {
-  console.log("Server running on http://localhost:5000");
-  const url = req.url;
-  const method = req.method;
-  console.log(url, method);
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html lang='en'>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html lang='en'>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
+  console.log(req.url, req.method);
+  routes(req, res);
 });
 
 server.listen(5000);
