8b7a74d01e8687292c411ac89f12797a26bb2126
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-20 18:38:07 +0200

    Create root route

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("Server running on http://localhost:5000")
+  const url = req.url;
+  console.log(url, req.method);
+  if (url === "/") {
+    res.setHeader('Content-Type', 'text/html');
+    res.write("<html lang='en'>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>");
+    res.write("</html>");
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write("<html lang='en'>");
   res.write("<head><title>My First Page</title></head>");
