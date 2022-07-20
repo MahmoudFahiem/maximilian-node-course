a8b18034cc2e600535fd37ba4cf34eaf1f25d0bd
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-20 21:11:51 +0200

    Event loop

--- server.js
+++ server.js
@@ -1,19 +1,6 @@
 const http = require("http");
 const fs = require("fs");
 
-/**
- * Streams and Buffers
- * - Stream is an ongoing process
- * - The request is simply read by node in chunks.
- * - It takes time to be fully parsed.
- * - This happens so that we - theoretically - can start working with those chunks without having to wait for the full request to be fully parsed
- * - Ex: a file being uploaded, it can take a lot of time to be fully parsed, in this case streaming of data makes sense,
- * - Since it could allow you to start writing this on disk while the data coming in so that you don't have to wait for the file to be fully parsed.
- * - With code you can't start working arbitrary with those chunks
- * - Instead you could organize these chunks using a Buffer
- * - A buffer is like bus stops, it holds multiple chunks and work with them before there are released once they are done
- */
-
 const server = http.createServer((req, res) => {
   console.log("Server running on http://localhost:5000");
   const url = req.url;
@@ -32,23 +19,19 @@ const server = http.createServer((req, res) => {
 
   if (url === "/message" && method === "POST") {
     const body = [];
-    /**
-     * Data event will be fired whenever a new chunk is ready to be read
-     */
+
     req.on("data", (chunk) => {
       body.push(chunk);
     });
-    /**
-     * End event will fire when the request is done
-     */
-    req.on("end", () => {
+
+    return req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
       const message = parseBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
 
   res.setHeader("Content-Type", "text/html");
