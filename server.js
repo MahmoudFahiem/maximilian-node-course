91608446195a4ae9c537b3b8c141dda1fd9abcd5
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-20 19:12:57 +0200

    Add redirection

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Server running on http://localhost:5000")
+  console.log("Server running on http://localhost:5000");
   const url = req.url;
-  console.log(url, req.method);
+  const method = req.method;
+  console.log(url, method);
   if (url === "/") {
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader("Content-Type", "text/html");
     res.write("<html lang='en'>");
     res.write("<head><title>My First Page</title></head>");
-    res.write("<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>"
+    );
     res.write("</html>");
     return res.end();
   }
-  res.setHeader('Content-Type', 'text/html');
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    // res.writeHead(382, {
+    //   "Location": "/"
+    // })
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
   res.write("<html lang='en'>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
-})
+});
 
-server.listen(5000)
+server.listen(5000);
