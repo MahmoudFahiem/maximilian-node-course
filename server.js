638426bf1183a9cf34e20db24145b06f6d215558
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-20 16:11:22 +0200

    End listening process

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("Server running on http://localhost:5000")
+  // Exist listening process
+  process.exit();
 })
 
 server.listen(5000)
